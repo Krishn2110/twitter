5a6cac470b8c8404c57eb6335d07b9b5596586d0
Krishn2110 <170015718+Krishn2110@users.noreply.github.com>
2024-07-23 10:12:19 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*html"],
+  theme: {
+    // colors:{
+    //         greymy: "#2f3336"
+    // },
+    extend: {},
+  },
+  plugins: [],
+}
+
